aa2d87f5f7e41c56bc9ab8903916d55355600a4a
Fiodar <siyankafiodar@gmail.com>
2022-04-29 21:50:30 +0300

    JS

---
+++ JS/script.js
@@ -0,0 +1,55 @@
+// // console.log("JS");
+
+// // let test = prompt("Test");
+
+// let message = 'Our massage';
+
+// // console.log(message);
+
+// alert(message);
+
+// let userName = prompt("Enter your name", "USER");
+
+// console.log(userName);
+
+// let isCorrectName = confirm("Имя введено верно?");
+
+// console.log(typeof(isCorrectName), isCorrectName);
+
+// let name = "IVAN";
+// let surname = "IVANOV";
+
+// name = surname;
+// console.log(name, surname);
+
+// // surname = name;
+// // console.log(name, surname);
+
+// let str = "Привет";
+// let str2 = 'Одинарные кавычки тоже подойдут';
+// let phrase = `Обратные кавычки позволяют встраивать переменные ${str}`;
+// let phrase2 = `Обратные кавычки позволяют встраивать переменные ${str}`;
+
+
+// console.log("str", str);
+// console.log("str2", str2)
+
+// console.log(phrase);
+// console.log(phrase2);
+
+// console.log(typeof(str));
+
+// let value = true;
+// console.log(value);
+
+// value = String(value); 
+
+// console.log(value);
+
+let str = "123O";
+
+console.log(str);
+let num = Number(str);
+console.log(num);
+
+

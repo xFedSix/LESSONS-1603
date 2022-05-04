b808586e83d48b106792bde21814495ff4273550
Fiodar <siyankafiodar@gmail.com>
2022-05-04 21:45:18 +0300

    Lesson2

---
+++ JS/Lesson2/script/script.js
@@ -0,0 +1,19 @@
+
+let a = 5;
+
+// if (a > 1) {
+//     console.log("больше");
+// } else if (a === 3) {
+//     console.log("равно");
+// } else {
+//     console.log("меньше")
+// }
+
+// let result = a > 5 ? "more" : a===5 ? "equal" : "less";
+
+// console.log(result);
+
+
+let result = (a + b < 4) ? "Мало" : "Много";
+
+console.log(result);

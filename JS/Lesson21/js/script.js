3b048b1af4ed2b5f0a3a6f18859bac007def8465
Fiodar <siyankafiodar@gmail.com>
2022-07-13 21:42:43 +0300

    main

--- JS/Lesson21/js/script.js
+++ JS/Lesson21/js/script.js
@@ -1,67 +1,81 @@
 // const debounce = (fn, ms) => {
-// 	let timeoutId;
-// 	return function () {
-// 		const fnCall = () => {fn.apply(this, arguments)}
-// 		clearTimeout(timeoutId)
-// 		timeoutId = setTimeout(fnCall, ms)
-// 	};
-	
+//     let timeoutId;
+//     return function () {
+//         const fnCall = () => { fn.apply(this, arguments) } 
+
+//         clearTimeout(timeoutId)
+
+//         timeoutId = setTimeout(fnCall, ms)
+//     };
 // }
 
-
 // function onChange(event) {
-// 	console.log(event.target.value);
+//     console.log(event.target.value);
 // }
 
 // onChange = debounce(onChange, 1000)
 
 // document.getElementById('search').addEventListener("keyup", onChange);
 
-// const number = 200;
-// const material = "block";
 
-// localStorage.setItem('blockMaterial', material);
+
+// const number = 200;
+// const material = "Block";
+
+// // console.log(localStorage.getItem('number'));
 
 // localStorage.setItem('number', number.toString());
-// localStorage.removeItem('number');
-// console.log(localStorage.getItem('number'));
-// localStorage.clear();
+// localStorage.setItem('blockMaterial', material);
 
-// const car ={
-// 	color: "red",
-// 	model: "Kia",
-// 	prize: 20000,
+// localStorage.removeItem("number")
+
+// localStorage.clear()
+
+
+// const car = {
+//     color: "green",
+//     model: "Kia",
+//     price: 44000,
 // }
 
-// localStorage.setItem("car", JSON.stringify(car));
+// localStorage.setItem('car', JSON.stringify(car));
 
 // const auto = localStorage.getItem('car');
+// // console.log(auto);
+
+// auto.color = "red";
+
+// console.log(auto);
 
 
 // window.addEventListener('storage', event => {
-// 	console.log(event);
+//     console.log(event);
 // })
+
+
+
 
 const dino = document.getElementById("dino");
 const cactus = document.getElementById("cactus");
 
 document.addEventListener("keydown", event => {
-	jump();
-})
+    jump();
+});
+
 function jump() {
-	if (dino.classList != "jump") {
-		dino.classList.add("jump")
-	}
-	setTimeout(function() {
-		dino.classList.remove('jump')
-	}, 800)
+    if (dino.classList != "jump") {
+        dino.classList.add("jump")
+    }
+    setTimeout(function() {
+        dino.classList.remove("jump")
+    }, 300)
 }
 
-let isAlive = setInterval(function () {
-	let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue('dino'));
-	let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue('cactus'));
+let isAlive = setInterval(function() {
+    let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+    let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
 
-	if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140) {
-		alert ("Game Over");
-	}
-},10)
+    if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140) {
+        alert("GAME OVER")
+    }
+}, 10)
